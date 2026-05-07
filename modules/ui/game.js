832c26289722b51a1bf7f8a61fff5de21069109f
james-e-crandall <james.e.crandall@gmail.com>
2026-05-06 20:50:21 -0500

    units, text

--- modules/ui/game.js
+++ modules/ui/game.js
@@ -1,15 +1,15 @@
 window.UnitPortraitVisualRemaps = {
     ...(window.UnitPortraitVisualRemaps || {}),
     'UNIT_STEPPE_HORSE_BOWMAN_JEC': { //your unit type
-        unitType: 'UNIT_HORSEMAN',
+        unitType: 'UNIT_KESHIG',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
     'UNIT_STEPPE_HORSE_BOWMAN_JEC_2': { //your unit type
-        unitType: 'UNIT_HORSEMAN',
+        unitType: 'UNIT_KESHIG',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
     'UNIT_STEPPE_HORSE_BOWMAN_JEC_3': { //your unit type
-        unitType: 'UNIT_HORSEMAN',
+        unitType: 'UNIT_KESHIG',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     }
 }
